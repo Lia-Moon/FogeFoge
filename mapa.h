863b1a9bfbad14da176aa735c1bfb7d6e19fdcfb
Júlia Vieira <112638042+Lia-Moon@users.noreply.github.com>
2022-11-04 14:49:53 -0300

    mapa.h

--- mapa.h
+++ mapa.h
@@ -9,7 +9,50 @@ struct mapa { // declarei uma estrutura de variáveis
 	// char mapa[5][10+1]; // o +1 serve para indicar o /0 que é o fim da matriz
 	char** matriz; // ** é um ponteiro de um ponteiro
 	int linhas;
+	int colunas;#ifndef _MAPA_H_
+#define _MAPA_H_
+
+#define HEROI '@'
+#define FANTASMA 'F'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define PILULA 'P'
+
+struct mapa { // declarei uma estrutura de variáveis
+	// matriz de 5 x 10, linhas x colunas
+	// char mapa[5][10+1]; // o +1 serve para indicar o /0 que é o fim da matriz
+	char** matriz; // ** é um ponteiro de um ponteiro
+	int linhas;
 	int colunas;
+}; // lembrar de terminar com ';' as structs
+
+typedef struct mapa MAPA; // typedef permite não precisar ficar usando a palavra struct, como se fosse um apelido pra struct
+
+struct posicao {
+	int x;
+	int y;
+};
+
+typedef struct posicao POSICAO;
+
+void liberamapa(MAPA* m);
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+int encontramapa(MAPA* m, POSICAO* p, char c);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA* m, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
+void copiamapa(MAPA* destino, MAPA* origem);
+
+int podeandar(MAPA* m, char personagem, int x, int y);
+
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+#endif
 }; // lembrar de terminar com ';'
 
 typedef struct mapa MAPA; // typedef permite não precisar ficar usando a palavra struct, como se fosse um apelido pra struct
